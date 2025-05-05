b0a6084c9a2bc896095f4216c8d9439fd5c75e81
Guénolé Choné <guenole.chone@concordia.ca>
2025-05-05 09:14:51 -0400

    Solver changed from fsolve (finding roots) to minimize (minimizing absolute difference of hydraulic heads).

--- Solver1Dnormal.py
+++ Solver1Dnormal.py
@@ -12,22 +12,24 @@ import warnings
 warnings.simplefilter("ignore", RuntimeWarning)
 
 from scipy.optimize import fsolve
+from scipy.optimize import minimize
 
 def manning_solver(cs):
 
 
     def equations(y):
         R = (cs.width * y) / (cs.width + 2 * y)
-        manning = (y * cs.width * R ** (2. / 3.) * cs.s ** 0.5) / cs.n - cs.Q
+        manning = (y * cs.width * R ** (2. / 3.) * cs.s_valid ** 0.5) / cs.n - cs.Q
         return manning
 
-    cs.y = fsolve(equations, 1)[0]
-    cs.R = (cs.width * cs.y) / (cs.width + 2 * cs.y)
-    cs.ycrit = (cs.Q / (cs.width * g ** 0.5)) ** (2. / 3.)
-    cs.v = cs.Q / (cs.width * cs.y)
-    cs.h = cs.z + cs.y + cs.v ** 2 / (2 * g)
+    cs.y_valid = fsolve(equations, 1)[0]
+    cs.R_valid = (cs.width * cs.y_valid) / (cs.width + 2 * cs.y_valid)
+    cs.ycrit_valid = (cs.Q / (cs.width * g ** 0.5)) ** (2. / 3.)
+    cs.v_valid = cs.Q / (cs.width * cs.y_valid)
+    cs.h_valid = cs.z + cs.y_valid + cs.v_valid ** 2 / (2 * g)
     #cs_tosolve.h = cs_tosolve.z + cs_tosolve.y
-    cs.Fr = cs.v / (g * cs.y) ** 0.5
+    cs.Fr_valid = cs.v_valid / (g * cs.y_valid) ** 0.5
+    cs.ws_valid = cs.z + cs.y_valid
 
 
 def cs_solver(cs_up, cs_down):
@@ -37,43 +39,44 @@ def cs_solver(cs_up, cs_down):
     cs_ref = cs_down
 
     if cs_down.reach == cs_up.reach:
-        cs_up.localdist = (cs_up.dist - cs_down.dist)
+        localdist = (cs_up.dist - cs_down.dist)
     else:
-        cs_up.localdist = cs_down.reach.length - cs_down.dist + cs_up.dist
+        localdist = cs_down.reach.length - cs_down.dist + cs_up.dist
+
+    # premier estimé : y = y_crit
+    cs_tosolve.ycrit_valid = (cs_tosolve.Q / (cs_tosolve.width * g ** 0.5)) ** (2. / 3.)
 
     def equations(y):
 
-        if y < cs_tosolve.ycrit:
-            # constraint simulation
-            return 9999
+        # if y < cs_tosolve.ycrit_valid:
+        #     # constraint simulation
+        #     return 9999
         R = (cs_tosolve.width * y) / (cs_tosolve.width + 2 * y)
         v = cs_tosolve.Q / (cs_tosolve.width * y)
         h = cs_tosolve.z + y
         # with kinetic energy?
         #h = h + (v ** 2) / (2 * g)
         s = (cs_tosolve.n ** 2 * v ** 2) / (R ** (4. / 3.))
-        friction_h = cs_up.localdist * s
-        #friction_h = cs_up.localdist * (s + cs_ref.s) / 2.
-        energy = cs_ref.h + friction_h - h
+        friction_h = localdist * s
+        #friction_h = localdist * (s + cs_ref.s) / 2.
+        energy = cs_ref.h_valid + friction_h - h
+        energy = abs(energy)
         return energy
 
-    # premier estimé : y = y_crit
-    cs_tosolve.ycrit = (cs_tosolve.Q / (cs_tosolve.width * g ** 0.5)) ** (2. / 3.)
-    #cs_tosolve.y = fsolve(equations, cs_tosolve.ycrit)[0]
-    res, dict, ier, msg = fsolve(equations, cs_tosolve.ycrit, full_output=True)
-    ## if ier != 1, an error occured.
-    if ier != 1:
-        cs_tosolve.y = 99
-    else:
-        cs_tosolve.y = res[0]  # actual result of the solver
+    res = minimize(equations, cs_tosolve.ycrit_valid, method='Nelder-Mead', bounds=[(cs_tosolve.ycrit_valid, None)], options={'xatol': 1e-3, 'fatol': 1e-3})
+    # res, dict, ier, msg = fsolve(equations, cs_tosolve.ycrit, full_output=True)
+    # ## if ier != 1, an error occured.
+    # if ier != 1:
+    #     cs_tosolve.y = 99
+    # else:
+    #     cs_tosolve.y = res[0]  # actual result of the solver
 
-    cs_tosolve.R = (cs_tosolve.width * cs_tosolve.y) / (cs_tosolve.width + 2 * cs_tosolve.y)
-    cs_tosolve.v = cs_tosolve.Q / (cs_tosolve.width * cs_tosolve.y)
+    cs_tosolve.y_valid = res.x[0]
+    cs_tosolve.R_valid = (cs_tosolve.width * cs_tosolve.y_valid) / (cs_tosolve.width + 2 * cs_tosolve.y_valid)
+    cs_tosolve.v_valid = cs_tosolve.Q / (cs_tosolve.width * cs_tosolve.y_valid)
     #cs_tosolve.h = cs_tosolve.z + cs_tosolve.y + cs_tosolve.v ** 2 / (2 * g)
-    cs_tosolve.h = cs_tosolve.z + cs_tosolve.y
-    cs_tosolve.s = (cs_tosolve.n ** 2 * cs_tosolve.v ** 2) / (cs_tosolve.R ** (4. / 3.))
-    cs_tosolve.Fr = cs_tosolve.v / (g * cs_tosolve.y) ** 0.5
-
-
-    del cs_up.localdist
+    cs_tosolve.h_valid = cs_tosolve.z + cs_tosolve.y_valid
+    cs_tosolve.s_valid = (cs_tosolve.n ** 2 * cs_tosolve.v_valid ** 2) / (cs_tosolve.R_valid ** (4. / 3.))
+    cs_tosolve.Fr_valid = cs_tosolve.v_valid / (g * cs_tosolve.y_valid) ** 0.5
+    cs_tosolve.ws_valid = cs_tosolve.z + cs_tosolve.y_valid
 
--- SolverDirect.py
+++ SolverDirect.py
@@ -8,9 +8,9 @@ import warnings
 warnings.simplefilter("ignore", RuntimeWarning)
 
 from scipy.optimize import fsolve
+from scipy.optimize import minimize
 
 def manning_solver(cs):
-
 
     def equations(y):
         R = (cs.width * y) / (cs.width + 2 * y)
@@ -36,9 +36,9 @@ def cs_solver(cs_up, cs_down, min_slope):
     cs_ref = cs_up
 
     if cs_down.reach == cs_up.reach:
-        localdist = (cs_up.dist - cs_down.dist)
+        localdist = float(cs_up.dist - cs_down.dist)
     else:
-        localdist = cs_down.reach.length - cs_down.dist + cs_up.dist
+        localdist = float(cs_down.reach.length - cs_down.dist + cs_up.dist)
 
 
     if (cs_up.wslidar - cs_down.wslidar)/localdist <= min_slope:
@@ -47,12 +47,15 @@ def cs_solver(cs_up, cs_down, min_slope):
     else:
         h_ref = cs_up.h
 
+    # premier estimé : y = y_crit
+    cs_tosolve.ycrit = (cs_tosolve.Q / (cs_tosolve.width * g ** 0.5)) ** (2. / 3.)
+
 
     def equations(y):
 
-        if y < cs_tosolve.ycrit:
-            # constraint simulation
-            return 9999
+        #if y < cs_tosolve.ycrit:
+        #    # constraint simulation
+        #    return 9999
         R = (cs_tosolve.width * y) / (cs_tosolve.width + 2 * y)
         v = cs_tosolve.Q / (cs_tosolve.width * y)
         s = (cs_tosolve.n ** 2 * v ** 2) / (R ** (4. / 3.))
@@ -63,18 +66,20 @@ def cs_solver(cs_up, cs_down, min_slope):
         #friction_h = localdist * (s+cs_ref.s)/2. # Friction can't be based on the average of slope, it leads to impossible to resolve cases
         friction_h = localdist * s
         energy = friction_h + h - h_ref
+        energy = abs(energy)# + abs(cs_tosolve.wslidar - y - cs_ref.z)/(10*localdist)
         return energy
 
-    # premier estimé : y = y_crit
-    cs_tosolve.ycrit = (cs_tosolve.Q / (cs_tosolve.width * g ** 0.5)) ** (2. / 3.)
 
-    res, dict, ier, msg = fsolve(equations, cs_tosolve.ycrit, full_output=True)
-    ## if ier != 1, an error occured.
-    if ier != 1:
-        cs_tosolve.y = 99
-    else:
-        cs_tosolve.y = res[0] # actual result of the solver
+    res = minimize(equations, cs_tosolve.ycrit, method='Nelder-Mead', bounds=[(cs_tosolve.ycrit, None)], options={'xatol':1e-3, 'fatol': 1e-3})
 
+    # Example of possible smoothing: y is changed according to local Fr and variation of Fr
+    # y = res.x[0]
+    # v = cs_tosolve.Q / (cs_tosolve.width * y)
+    # Fr = v / (g * y) ** 0.5
+    # weight = max(Fr, min(2*(Fr-cs_up.Fr)/cs_up.Fr, 1))
+    # cs_tosolve.y = weight*y + (1-weight)*cs_up.y
+
+    cs_tosolve.y = res.x[0]
     cs_tosolve.R = (cs_tosolve.width * cs_tosolve.y) / (cs_tosolve.width + 2 * cs_tosolve.y)
     cs_tosolve.v = cs_tosolve.Q / (cs_tosolve.width * cs_tosolve.y)
     cs_tosolve.z = cs_tosolve.wslidar - cs_tosolve.y
@@ -86,7 +91,9 @@ def cs_solver(cs_up, cs_down, min_slope):
 
     cs_tosolve.Fr = cs_tosolve.v / (g * cs_tosolve.y) ** 0.5
 
-    return ier
+
+
+    return res
 
 
 
